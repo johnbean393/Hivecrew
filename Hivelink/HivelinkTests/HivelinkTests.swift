92d0519601aedec0f571ef056129c35b08f8abf2
johnbean393 <johnbean393@gmail.com>
2026-04-14 13:11:47 -0400

    feat: Add Hivelink

---
+++ Hivelink/HivelinkTests/HivelinkTests.swift
@@ -0,0 +1,19 @@
+//
+//  HivelinkTests.swift
+//  HivelinkTests
+//
+//  Created by John Bean on 4/14/26.
+//
+
+import Testing
+@testable import Hivelink
+
+struct HivelinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
